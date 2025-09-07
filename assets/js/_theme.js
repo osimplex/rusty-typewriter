a84c9054584ac98fa5db677ffd6be164c5ffd10b
math-queiroz <m-alves7@hotmail.com>
2025-09-06 21:45:03 -0300

    fix: no more FOUC and simplified _theme.js

--- assets/js/_theme.js
+++ assets/js/_theme.js
@@ -2,37 +2,20 @@
     "use strict";
 
     const getCachedTheme = () => {
-      const theme = document.body.getAttribute("color-scheme");
       const cachedTheme = localStorage.getItem("color-scheme");
-      if (cachedTheme) {
-        return cachedTheme;
-      } else if (theme !== "dark" && theme !== "light") {
-        let preferDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-        return preferDark ? "dark" : "light";
-      } else {
-        return theme
-      }
+      const preferDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      return cachedTheme || (preferDark ? "dark" : "light")
     };
 
-    const toggle = (state) => state === "light" ? "dark" : "light";
-    const initTheme = (state) => {
-      document.documentElement.setAttribute("color-scheme", state);
-    };
-
-    const toggleTheme = () => {
-      const state = getCachedTheme();
-        localStorage.setItem("color-scheme", toggle(state));
-        initTheme(toggle(state));
-    };
+    document.documentElement.setAttribute("color-scheme", getCachedTheme());
 
     window.addEventListener("DOMContentLoaded", () => {
-      initTheme(getCachedTheme());
       requestAnimationFrame(() => document.body.classList.remove("notransition"))
-
-      const switcher = document.getElementById("theme-switcher");
-      switcher.addEventListener("click", e => {
+      document.getElementById("theme-switcher").addEventListener("click", e => {
         e.preventDefault();
-        toggleTheme()
+        const state = getCachedTheme() === "light" ? "dark" : "light";
+        document.documentElement.setAttribute("color-scheme", state);
+        localStorage.setItem("color-scheme", state);
       });
     });
 })();
